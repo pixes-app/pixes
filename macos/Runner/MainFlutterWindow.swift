76fa2c907696fe01e80e0f01cbb136a24c084330
nyne <ynyx89@gmail.com>
2026-04-04 19:30:08 +0800

    Update build script for macos

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -12,16 +12,16 @@ class MainFlutterWindow: NSWindow {
     let proxyChannel = FlutterMethodChannel(
       name: "pixes/proxy",
       binaryMessenger: flutterViewController.engine.binaryMessenger)
-    proxyChannel.setMethodCallHandler { (call, result) in
+      proxyChannel.setMethodCallHandler { (call, result) in
       // 获取代理设置
         if let proxySettings = CFNetworkCopySystemProxySettings()?.takeUnretainedValue() as NSDictionary?,
-             let dict = proxySettings.object(forKey: kCFNetworkProxiesHTTPProxy) as? NSDictionary,
-             let host = dict.object(forKey: kCFNetworkProxiesHTTPProxy) as? String,
-             let port = dict.object(forKey: kCFNetworkProxiesHTTPPort) as? Int {
-            let proxyConfig = "\(host):\(port)"
-            result(proxyConfig)
+          let dict = proxySettings.object(forKey: kCFNetworkProxiesHTTPProxy) as? NSDictionary,
+          let host = dict.object(forKey: kCFNetworkProxiesHTTPProxy) as? String,
+          let port = dict.object(forKey: kCFNetworkProxiesHTTPPort) as? Int {
+          let proxyConfig = "\(host):\(port)"
+          result(proxyConfig)
         } else {
-            result("No proxy")
+          result("No proxy")
         }
     }
 
