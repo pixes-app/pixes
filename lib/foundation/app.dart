b172388b0a6071087956f06b5cb662c0dde1d6d9
ynyxx <239976302+ynyxx@users.noreply.github.com>
2026-07-12 17:09:30 +0800

    Update version code

--- lib/foundation/app.dart
+++ lib/foundation/app.dart
@@ -12,7 +12,7 @@ export "state_controller.dart";
 export "navigation.dart";
 
 class _App {
-  final version = "1.2.2";
+  final version = "1.2.3";
 
   bool get isAndroid => Platform.isAndroid;
   bool get isIOS => Platform.isIOS;
